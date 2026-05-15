55f46e27007b69947e712468a53d67fa1ffd579c
STCautotrade <stcautotradepro@gmail.com>
2026-05-16 06:12:36 +0700

    Implement on_pre_leave method in Trade class
    Add on_pre_leave method to navigate to home screen

--- trade.py
+++ trade.py
@@ -5,9 +5,15 @@ Intent = autoclass('android.content.Intent')
 Uri = autoclass('android.net.Uri')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
+
 class Trade(Screen):
 
     def on_enter(self):
         intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://stockity.com"))
         currentActivity = PythonActivity.mActivity
         currentActivity.startActivity(intent)
+
+    def on_pre_leave(self):
+        # saat mau keluar dari Trade → paksa balik HOME
+        if self.manager:
+            self.manager.current = "home"
